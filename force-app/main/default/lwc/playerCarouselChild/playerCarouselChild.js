2cc6c7293da60ecd9d70f239e3252cce2629810d
ArshadSDev <arshad17006@gmail.com>
2026-05-01 01:01:49 +0530

    learnings 01-05

--- force-app/main/default/lwc/playerCarouselChild/playerCarouselChild.js
+++ force-app/main/default/lwc/playerCarouselChild/playerCarouselChild.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class PlayerCarouselChild extends LightningElement {}
+export default class PlayerCarouselChild extends LightningElement {
+    
+
+    _playerDetails;
+    @api
+    get playerDetailsChild() {
+        return this._playerDetails;
+    }
+    set playerDetailsChild(value) {
+         this._playerDetails = value.map(player => ({...player,name: player.name.toUpperCase()}));
+    }
+
+    get teamCheck() {
+        return this.playerDetailsChild.team === 'CSK';
+    }
+}
--- force-app/main/default/lwc/playerCarouselPar/playerCarouselPar.js
+++ force-app/main/default/lwc/playerCarouselPar/playerCarouselPar.js
@@ -1,9 +1,57 @@
 import { LightningElement } from 'lwc';
+import playerImage from '@salesforce/resourceUrl/playerImage';
+import brewis from '@salesforce/resourceUrl/brewis';
+import sanjuSamson from '@salesforce/resourceUrl/sanjuSamson';
+import hardikPandya from '@salesforce/resourceUrl/hardikPandya';
+import rohitSharma from '@salesforce/resourceUrl/rohitSharma';
+import tilakVarma from '@salesforce/resourceUrl/tilakVarma';
+import Description from '@salesforce/schema/Account.Description';
 
 export default class PlayerCarouselPar extends LightningElement {
 
     playerDetails = [
         {
-            id: 1
-        }]
+            id: 1,
+            name : 'Dhoni',
+            team : 'CSK',
+            image : playerImage,
+            Description : 'MS Dhoni is a former Indian cricketer and captain.'
+        },  
+    {
+            id: 2,
+            name: 'sanju samson',
+            team: 'CSK',
+            image: sanjuSamson,
+            Description: 'Sanju Samson is an Indian cricketer and wicket-keeper.'
+        },
+        {
+            id: 3,
+            name: 'brevis',
+            team: 'CSK',
+            image: brewis,
+            Description: 'Brewis is an Indian cricketer.'
+        },
+        {
+            id: 4,
+            name: 'Rohit Sharma',
+            team: 'MI',
+            image: rohitSharma,
+            Description: 'Rohit Sharma is an Indian cricketer and captain.'
+        },
+        {
+            id: 5,
+            name: 'hardik pandya',
+            team: 'MI',
+            image: hardikPandya,
+            Description: 'Hardik Pandya is an Indian cricketer.'
+        },
+        {
+            id: 6,
+            name: 'Tilak',
+            team: 'MI',
+            image: tilakVarma,
+            Description: 'Tilak Varma is an Indian cricketer.'
+        }
+       
+    ];
 }
