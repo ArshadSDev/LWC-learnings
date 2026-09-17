43ec26309e10403d368cb4c8d6d591f0753ceb24
ArshadSDev <arshad17006@gmail.com>
2026-09-17 15:23:04 +0530

    till wire object adapter

--- force-app/main/default/lwc/wireContactDemo/wireContactDemo.js
+++ force-app/main/default/lwc/wireContactDemo/wireContactDemo.js
@@ -1,13 +1,28 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
+import Contact_OBJECT from "@salesforce/schema/Contact";
+import Email_FIELD from '@salesforce/schema/Contact.Email';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import Phone_FIELD from '@salesforce/schema/Contact.Phone';
 
 export default class WireContactDemo extends LightningElement {
 
-    @wire(getRecord,{recordId:'003dM000024DwsVQAS',fields:['Contact.LastName','Contact.Email']})
+    @api recordId;
+    phone;
+    email;
+    lastName;
+
+    contactFields={};
+
+    @wire(getRecord,{recordId:'$recordId',fields:[Email_FIELD,LASTNAME_FIELD,Phone_FIELD]})
     contactDetailsFun({data,error}){
         if(data)
-        {
-            console.log(data);
+        {   
+            this.email= data.fields.Email.value;
+            this.phone = data.fields.Phone.value;
+            this.lastName = data.fields.LastName.value;
+            console.log('from record api',data);
             console.log('Email :' + data.fields.Email.value);
             console.log('LastName :'+data.fields.LastName.value);
         }
@@ -16,4 +31,30 @@ export default class WireContactDemo extends LightningElement {
             console.error(error);
         }
     }
+    @wire (getObjectInfo, { objectApiName: Contact_OBJECT }) 
+    ContactobjectInfo({data,error}){
+        if(data)
+        {   
+            this.contactFields = data;
+            console.log('from object api', data);
+        }
+        else if(error)
+        {
+            console.error(error);
+        }
+    }
+    get fieldsArray() {
+    return this.contactFields.fields 
+        ? Object.values(this.contactFields.fields) 
+        : [];
+    }
+    get fieldsCount() {
+        return this.fieldsArray.length;
+    }
+
+    get customfieldcount(){
+        return this.fieldsArray.filter(field => field.custom).length;
+    }
+    
+    
 }
